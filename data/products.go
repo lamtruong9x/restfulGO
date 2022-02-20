6e8f83648945f8924b6076f14fe9f7ee2573311e
truong98 <damlamtruong@gmail.com>
2022-02-20 20:03:43 +0700

    fixed price tag

--- data/products.go
+++ data/products.go
@@ -15,7 +15,7 @@ type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
-	Price       float32 `json:"price" validate:gt=0`
+	Price       float32 `json:"price" validate:"gt=0"`
 	SKU         string  `json:"sku" validate:"required,sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
